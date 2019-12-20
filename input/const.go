c8ae435cd06e240f1377b7f08fafb7a05d573de5
HAYAMA_Kaoru <iyahaya@nifty.com>
2019-12-21 08:58:21 +0900

    Use github.com/zetamatta/go-importconst

--- input/const.go
+++ input/const.go
@@ -1,13 +1,15 @@
 package consoleinput
 
-const FOCUS_EVENT = 16
-const KEY_EVENT = 1
-const MENU_EVENT = 8
-const MOUSE_EVENT = 2
-const WINDOW_BUFFER_SIZE_EVENT = 4
-const ENABLE_WINDOW_INPUT = 8
-const WAIT_ABANDONED = 128
-const WAIT_OBJECT_0 = 0
-const WAIT_TIMEOUT = 258
-const WAIT_FAILED = -1
-const ENABLE_MOUSE_INPUT = 16
+//go:generate go run github.com/zetamatta/go-importconst/
+//	<windows.h>
+//	FOCUS_EVENT
+//	KEY_EVENT
+//	MENU_EVENT
+//	MOUSE_EVENT
+//	WINDOW_BUFFER_SIZE_EVENT
+//	ENABLE_WINDOW_INPUT
+//	WAIT_ABANDONED
+//	WAIT_OBJECT_0
+//	WAIT_TIMEOUT
+//	WAIT_FAILED
+//	ENABLE_MOUSE_INPUT
